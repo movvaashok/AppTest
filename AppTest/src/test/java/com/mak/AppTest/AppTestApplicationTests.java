d24122425e5996f5ff0b2fb8f6a12bff2c84ecba
Ashok Movva <ashokmovva555@gmail.com>
2020-05-05 16:14:22 +0100

    default classes and packages

---
+++ AppTest/src/test/java/com/mak/AppTest/AppTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mak.AppTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
